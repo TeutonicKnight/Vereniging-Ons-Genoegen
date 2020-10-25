2c9dde57f3f04392f39074ebbc9463b8dc38abb1
TeutonicKnight <kyberkittie@protonmail.com>
2020-10-25 16:03:19 +0100

    slight db changes

--- website/config.py
+++ website/config.py
@@ -3,4 +3,4 @@
 class Config:
 	# Three /// creates a realative path alongside the module, so will place inside same folder
 	SECRET_KEY = '352fd3d0f45582d3c5642c75ada5c408'
-	SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@localhost:5432/ons_genoegen"
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
